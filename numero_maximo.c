5cacadbf2bf2c635fdb99d595187b566260747dc
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-03-13 03:04:26 -0600

    Create numero_maximo.c

---
+++ numero_maximo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <float.h>
+
+int main() {
+    float numero_maximo = FLT_MAX;
+    float suma = numero_maximo;
+    
+    printf("El máximo número flotante que se puede mostrar en una suma es: %e", suma);
+    
+    return 0;
+}
